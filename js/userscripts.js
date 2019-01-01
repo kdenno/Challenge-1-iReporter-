47d788a068384eb366997b39bc00540f596149a1
kdenno <kabuyedennis3@gmail.com>
2019-01-01 21:28:44 +0300

    update userscripts.js to ES6 classes

--- js/userscripts.js
+++ js/userscripts.js
@@ -1,58 +1,38 @@
-        // show overlay    
-        let postTitle;
-        let postContent;
-        let overlaydiv = document.querySelector('.irepoverlay');
+        let ui = new UI;
+
         document.addEventListener('click', (e) => {
-            if (e.target.className === 'editpost') {
+            let classnames = e.target.className;
+            switch (classnames) {
+                case 'editpost':
+                    let titlenode = e.target.parentElement.parentElement.parentElement.parentElement.children[0];
+                    let post_content_node = e.target.parentElement.parentElement.parentElement.parentElement.children[1];
+                    e.preventDefault();
+                    ui.showEditForm(titlenode, post_content_node)
+                    break;
 
-                e.preventDefault();
+                case 'closeoverlay':
+                    ui.closeOverlay();
+                    break;
 
-                postTitle = (e.target.parentElement.parentElement.parentElement.parentElement.children[0]);
-                postContent = (e.target.parentElement.parentElement.parentElement.parentElement.children[1]);
-                // update text field
-                document.querySelector('#editpost_input').value = postTitle.innerText;
-                // update text area
-                document.querySelector('#editpost_textarea').value = postContent.innerText;
+                case 'update':
+                    e.preventDefault();
+                    ui.ShowUpdate();
+                    break;
 
-
-                let newclass = overlaydiv.className + ' is-visible';
-                overlaydiv.setAttribute('class', newclass)
+                case 'deletepost':
+                    let parentnode = e.target.parentNode.parentNode.parentNode.parentNode;
+                    ui.deletePost(parentnode)
+                    break;
             }
 
-        });
-        // show update
-        document.getElementById('update').addEventListener('click', (e) => {
-            e.preventDefault();
-            // update
-            postTitle.innerText = '';
-            postTitle.innerText = document.querySelector('#editpost_input').value
-            postContent.innerText = document.querySelector('#editpost_textarea').value
-            overlaydiv.setAttribute('class', 'irepoverlay')
 
-        });
-
-
-        // close overlay
-        document.querySelector('.closeoverlay').addEventListener('click', () => {
-            overlaydiv.setAttribute('class', 'irepoverlay')
-        });
-        // delete post 
-        let deleteArr = document.querySelectorAll('.deletepost');
-        deleteArr.forEach(element => {
-            element.addEventListener('click', (e) => {
-                e.target.parentNode.parentNode.parentNode.parentNode.remove();
-
-            })
         });
 
         // get flag posts 
-        let flagdataArr = document.querySelectorAll('.flagdata');
-        flagdataArr.forEach((element) => {
-            element.addEventListener('click', (e) => {
-                if (e.target.classList.contains('statheader1') || e.target.classList.contains('statheader2')) {
-                    document.querySelector('.siteheading').textContent = (e.target.childNodes[0].data)
 
-                }
+        document.addEventListener('click', (e) => {
+            if (e.target.classList.contains('statheader1') || e.target.classList.contains('statheader2')) {
+                document.querySelector('.siteheading').textContent = (e.target.childNodes[0].data)
 
-            })
-        });
+            }
+        })
